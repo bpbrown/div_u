f2a2e14b72a35e9666144e048a79749dd82cacde
Benjamin Brown <bpbrown@gmail.com>
2024-05-15 20:33:33 -0700

    First order form, from working with Dedalus team in SLO

---
+++ rb_first.py
@@ -0,0 +1,157 @@
+"""
+Dedalus script simulating 2D horizontally-periodic Rayleigh-Benard convection.
+This script demonstrates solving a 2D Cartesian initial value problem. It can
+be ran serially or in parallel, and uses the built-in analysis framework to save
+data snapshots to HDF5 files. The `plot_snapshots.py` script can be used to
+produce plots from the saved data. It should take about 5 cpu-minutes to run.
+
+The problem is non-dimensionalized using the box height and freefall time, so
+the resulting thermal diffusivity and viscosity are related to the Prandtl
+and Rayleigh numbers as:
+
+    kappa = (Rayleigh * Prandtl)**(-1/2)
+    nu = (Rayleigh / Prandtl)**(-1/2)
+
+For incompressible hydro with two boundaries, we need two tau terms for each the
+velocity and buoyancy. Here we choose to use a first-order formulation, putting
+one tau term each on auxiliary first-order gradient variables and the others in
+the PDE, and lifting them all to the first derivative basis. This formulation puts
+a tau term in the divergence constraint, as required for this geometry.
+
+To run and plot using e.g. 4 processes:
+    $ mpiexec -n 4 python3 rayleigh_benard.py
+    $ mpiexec -n 4 python3 plot_snapshots.py snapshots/*.h5
+"""
+
+import numpy as np
+import dedalus.public as d3
+import logging
+logger = logging.getLogger(__name__)
+
+
+# Parameters
+Lx, Lz = 4, 1
+Nx, Nz = 256, 128
+Rayleigh = 2e6
+Prandtl = 1
+dealias = 3/2
+stop_sim_time = 50
+timestepper = d3.RK222
+max_timestep = 0.125
+dtype = np.float64
+case = 'first'
+
+# Bases
+coords = d3.CartesianCoordinates('x', 'z')
+dist = d3.Distributor(coords, dtype=dtype)
+xbasis = d3.RealFourier(coords['x'], size=Nx, bounds=(0, Lx), dealias=dealias)
+zbasis = d3.ChebyshevT(coords['z'], size=Nz, bounds=(0, Lz), dealias=dealias)
+
+# Fields
+p = dist.Field(name='p', bases=(xbasis,zbasis))
+b = dist.Field(name='b', bases=(xbasis,zbasis))
+u = dist.VectorField(coords, name='u', bases=(xbasis,zbasis))
+tau_c1 = dist.Field(name='tau_c1')
+tau_c2 = dist.Field(name='tau_c2')
+tau_c3 = dist.Field(name='tau_c3')
+tau_b1 = dist.Field(name='tau_b1', bases=xbasis)
+tau_b2 = dist.Field(name='tau_b2', bases=xbasis)
+tau_u1 = dist.VectorField(coords, name='tau_u1', bases=xbasis)
+tau_u2 = dist.VectorField(coords, name='tau_u2', bases=xbasis)
+tau_u3 = dist.VectorField(coords, name='tau_u3', bases=xbasis)
+#taus = [tau_c1, tau_c2, tau_c3, tau_b1, tau_b2, tau_u1, tau_u2, tau_u3]
+taus = [tau_c1, tau_b1, tau_b2, tau_u1, tau_u2]
+
+# Substitutions
+kappa = (Rayleigh * Prandtl)**(-1/2)
+nu = (Rayleigh / Prandtl)**(-1/2)
+x, z = dist.local_grids(xbasis, zbasis)
+ex, ez = coords.unit_vector_fields(dist)
+V = Lx*Lz
+volavg = lambda A: d3.integ(A)/V
+xavg = lambda A: d3.integ(A,'x')/Lx
+
+lift_basis = zbasis.derivative_basis(1)
+lift = lambda A: d3.Lift(A, lift_basis, -1)
+grad_u = d3.grad(u) + ez*lift(tau_u1) # First-order reduction
+grad_b = d3.grad(b) + ez*lift(tau_b1) # First-order reduction
+
+tau_div = tau_c1
+tau_b = lift(tau_b2)
+tau_u = lift(tau_u2)
+
+# Problem
+# First-order form: "div(f)" becomes "trace(grad_f)"
+# First-order form: "lap(f)" becomes "div(grad_f)"
+
+# Problem
+vars = [p, b, u]
+problem = d3.IVP(vars + taus, namespace=locals())
+problem.add_equation("trace(grad_u) + tau_div = 0")
+problem.add_equation("dt(b) - ez@u - kappa*div(grad_b) + tau_b = - u@grad(b)")
+problem.add_equation("dt(u) - nu*div(grad_u) + grad(p) - b*ez + tau_u = - u@grad(u)")
+problem.add_equation("b(z=0) = 0")
+problem.add_equation("u(z=0) = 0")
+problem.add_equation("b(z=Lz) = 0")
+problem.add_equation("u(z=Lz) = 0")
+problem.add_equation("integ(p) = 0") # Pressure gauge
+
+# Solver
+solver = problem.build_solver(timestepper, enforce_real_cadence=np.inf)
+solver.stop_sim_time = stop_sim_time
+
+# Initial conditions
+b.fill_random('g', seed=42, distribution='normal', scale=1e-3) # Random noise
+b['g'] *= z * (Lz - z) # Damp noise at walls
+#b['g'] += Lz - z # Add linear background
+b.low_pass_filter(scales=0.25)
+
+# Analysis
+snapshots = solver.evaluator.add_file_handler(case+'/snapshots', iter=100)
+snapshots.add_task(b, name='buoyancy')
+snapshots.add_task(-d3.div(d3.skew(u)), name='vorticity')
+snapshots.add_task(d3.div(u), name='divergence')
+
+scalars = solver.evaluator.add_file_handler(case+'/scalars', iter=10)
+scalars.add_task(volavg(np.sqrt(u@u)/nu), name='Re')
+scalars.add_task(volavg(d3.div(u)), name='div_u')
+scalars.add_task(volavg(np.abs(d3.div(u))), name='|div_u|')
+scalars.add_task(np.abs(tau_c1), name='tau_c1')
+scalars.add_task(xavg(np.abs(tau_b1)), name='tau_b1')
+scalars.add_task(xavg(np.abs(tau_b2)), name='tau_b2')
+scalars.add_task(xavg(np.sqrt(tau_u1@tau_u1)), name='tau_u1')
+scalars.add_task(xavg(np.sqrt(tau_u2@tau_u2)), name='tau_u2')
+scalars.add_task(np.abs(tau_div), name='tau_div')
+scalars.add_task(np.sqrt(volavg(tau_b**2)), name='tau_b')
+scalars.add_task(np.sqrt(volavg(tau_u@tau_u)), name='tau_u')
+
+# CFL
+CFL = d3.CFL(solver, initial_dt=max_timestep, cadence=10, safety=0.5, threshold=0.05,
+             max_change=1.5, min_change=0.5, max_dt=max_timestep)
+CFL.add_velocity(u)
+
+# Flow properties
+flow = d3.GlobalFlowProperty(solver, cadence=10)
+flow.add_property(np.sqrt(u@u)/nu, name='Re')
+flow.add_property(np.abs(d3.div(u)), name='|div_u|')
+
+# Main loop
+startup_iter = 10
+try:
+    logger.info('Starting main loop')
+    while solver.proceed:
+        timestep = CFL.compute_timestep()
+        solver.step(timestep)
+        if (solver.iteration-1) % 1000 == 0:
+            for var in vars:
+                var['g']
+                var['c']
+        if (solver.iteration-1) % 10 == 0:
+            max_Re = flow.max('Re')
+            max_divu = flow.max('|div_u|')
+            logger.info(f'Iteration={solver.iteration:d}, Time={solver.sim_time:.2e}, dt={timestep:.2e}, max(Re)={max_Re:.2e}, divu={max_divu:.2e}')
+except:
+    logger.error('Exception raised, triggering end of main loop.')
+    raise
+finally:
+    solver.log_stats()
